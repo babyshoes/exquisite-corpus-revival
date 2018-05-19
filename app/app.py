9efec68526975dd952edaeaf65ba4d86678718e3
babyshoes <dianecai@gmail.com>
2018-05-19 18:28:05 -0400

    fresh start

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def for_starts():
+    return "hello world"
+    
+if __name__ == 'main':
+    app.run(debug=True, host='0.0.0.0')
